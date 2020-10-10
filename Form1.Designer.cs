062352840cc7045995e811d08dff9f52718ba776
kasa500 <72657796+kasa500@users.noreply.github.com>
2020-10-10 16:33:38 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace autoclicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_start = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.button_stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.interval_value = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.interval_value)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(34, 42);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(75, 23);
+            this.button_start.TabIndex = 0;
+            this.button_start.Text = "START";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // button_stop
+            // 
+            this.button_stop.Location = new System.Drawing.Point(34, 71);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(75, 23);
+            this.button_stop.TabIndex = 1;
+            this.button_stop.Text = "STOP";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Interval(ms):";
+            // 
+            // interval_value
+            // 
+            this.interval_value.Location = new System.Drawing.Point(92, 13);
+            this.interval_value.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.interval_value.Name = "interval_value";
+            this.interval_value.Size = new System.Drawing.Size(38, 23);
+            this.interval_value.TabIndex = 4;
+            this.interval_value.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(141, 108);
+            this.Controls.Add(this.interval_value);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.button_start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sonic Clicker";
+            ((System.ComponentModel.ISupportInitialize)(this.interval_value)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.NumericUpDown interval_value;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace autoclicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool toggle = false;
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (Control.ModifierKeys == Keys.Alt)
+            {
+                if (toggle == false)
+                {
+                    toggle = true;
+                }
+                else
+                {
+                    toggle = false;
+                }
+
+                Thread.Sleep(50);
+            }
+
+            if (toggle == true)
+            {
+                DoMouseClick();
+            }
+
+                Make_click();
+        }
+        private void Make_click()
+        {
+            if (Control.ModifierKeys == Keys.Control)
+            {
+                DoMouseClick();
+            }
+        }
+
+        private void Stop_clicker()
+        {
+            timer.Enabled = false;
+            button_start.Enabled = true;
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hold CTRL to click FAST!\r\n Press ALT to TOGGLE.");
+            start_clicker();
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            Stop_clicker();
+        }
+
+        private void start_clicker()
+        {
+            button_start.Enabled = false;
+            timer.Interval = (int)interval_value.Value;
+            timer.Enabled = true;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        public void DoMouseClick()
+        {
+            uint X = (uint)Cursor.Position.X;
+            uint Y = (uint)Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+    }
+}
